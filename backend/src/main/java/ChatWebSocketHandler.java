e6c2bf732dca49d015c0a83761baf6d9801722c3
Gordon Clark <gmclark@hotmail.co.uk>
2018-10-27 22:02:27 +0100

    a bit more of a start

--- backend/src/main/java/ChatWebSocketHandler.java
+++ backend/src/main/java/ChatWebSocketHandler.java
@@ -1,3 +1,26 @@
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.api.Session;
+
+@WebSocket
 public class ChatWebSocketHandler {
 
+    @OnWebSocketConnect
+    public void onConnect(Session user){
+        Server.sendMessage(user, "You have connected");
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session user, int status, String msg){
+        Server.sendMessage(user, "You have disconnected from the server");
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session user, String msg){
+        // String reply = chatBot.poll(msg);
+        String reply = "reply";     //ToDo: Poll Chat bot
+        Server.sendMessage(user, reply);
+    }
 }
--- backend/src/main/java/Server.java
+++ backend/src/main/java/Server.java
@@ -1,20 +1,25 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.jetty.websocket.api.Session;
 import org.json.JSONObject;
 
-import static j2html.TagCreator.*;
 import static spark.Spark.*;
 
 public class Server {
+
     public static void main(String[] args) {
         staticFileLocation("");
         webSocket("/chat", ChatWebSocketHandler.class);
         init();
     }
 
-//    public st
+    public static void sendMessage(Session sender, String msg){
+        try{
+            sender.getRemote().sendString(String.valueOf(new JSONObject().put("msg", msg)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
