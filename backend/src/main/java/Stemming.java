e31d140e65ab92c212ce72e46b28c7383d54c521
Cameron Lamberth <cammylamberth1997@hotmail.co.uk>
2018-10-28 01:05:34 +0100

    Implemented removing stop words and stemming

---
+++ backend/src/main/java/Stemming.java
@@ -0,0 +1,32 @@
+import edu.stanford.nlp.simple.Sentence;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Stemming {
+    public static List<String> getStems(String sentence){
+        Sentence s = new Sentence(sentence.toLowerCase());
+        List<String> lemmas = s.lemmas();
+        HashSet<String> stopWords = new HashSet<>();
+        sentence = "";
+        try (Stream<String> stream = Files.lines(Paths.get("backend/stopwords.txt"))) {
+            stream.forEach(stopWords::add);
+            for (String lemma : lemmas) {
+                if (!stopWords.contains(lemma))
+                    sentence = sentence.concat(lemma + " ");
+            }
+        } catch (IOException ignored) {}
+
+
+        return new Sentence(sentence).lemmas();
+}
+
+}
